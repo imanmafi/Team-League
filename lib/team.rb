4df4e5afd108b3e8a7bb05d37740540210dac75b
andrew dang & iman mafi <student@epicodus.com>
2015-08-24 12:11:07 -0700

    add tournament method to teams class

--- lib/team.rb
+++ lib/team.rb
@@ -2,4 +2,6 @@ class Team < ActiveRecord::Base
   scope(:disqualified, -> do
     where({:disqualified => false})
   end)
+
+  belongs_to(:tournament)
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -11,4 +11,12 @@ describe(Team) do
       expect(Team.disqualified()).to(eq(disqualified_teams))
     end
   end
+
+  describe("#tournament") do
+    it('tells which tournament the team belongs to') do
+      test_tournament = Tournament.create({:tournament_name => "city tournament"})
+      test_team = Team.create({:team_name => "Moms of Beaverton", :tournament_id => test_tournament.id})
+      expect(test_team.tournament()).to(eq(test_tournament))
+    end
+  end
 end
